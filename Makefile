c9abb973f6e1a9b5df48633185b46a2629f8f863
s.andreyuk <s.andreyuk@crpt.ru>
2022-05-16 18:25:21 +0300

    - picocli adding

---
+++ Makefile
@@ -0,0 +1,3 @@
+# Makefile
+run-dist: # запуск исполняемого файла
+	./build/install/app/bin/app
--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,24 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Option;
+
+@CommandLine.Command(name = "App", header = "Compares two configuration files and shows a difference.", version = "1.0")
 public class App {
+
+    @Option(names = { "-h", "--help" }, usageHelp = true, description = "Show this help message and exit.")
+    static boolean usageHelpRequested = false;
+
+    @Option(names = { "-v", "--version" }, versionHelp = true, description = "Print version information and exit.")
+    boolean versionRequested;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        CommandLine commandLine = new CommandLine(new App());
+        commandLine.parseArgs(args);
+        if (commandLine.isUsageHelpRequested()) {
+            commandLine.usage(System.out);
+        } else if (commandLine.isVersionHelpRequested()) {
+            commandLine.printVersionHelp(System.out);
+        }
     }
 }
