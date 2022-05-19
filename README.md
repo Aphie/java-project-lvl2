a492ccfe526ac27890e6394f75c2567314e65975
s.andreyuk <s.andreyuk@crpt.ru>
2022-05-19 23:05:31 +0300

    - add Differ class

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Aphie/java-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/Aphie/java-project-lvl2/actions)
+
+Сверка двух файлов:
+
--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -2,23 +2,48 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Option;
+import hexlet.code.Differ;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.Callable;
 
 @CommandLine.Command(name = "App", header = "Compares two configuration files and shows a difference.", version = "1.0")
-public class App {
+public class App implements Callable<String> {
+
+    @CommandLine.Parameters(paramLabel = "filepath1", description = "path to first file")
+    Path filepath1;
+
+    @CommandLine.Parameters(paramLabel = "filepath2", description = "path to second file")
+    Path filepath2;
+
+    @Option(names = { "-f", "--format" }, defaultValue = "stylish", description = "output format [default: stylish]")
+    String format;
 
     @Option(names = { "-h", "--help" }, usageHelp = true, description = "Show this help message and exit.")
-    static boolean usageHelpRequested = false;
+    boolean usageHelpRequested = false;
 
     @Option(names = { "-v", "--version" }, versionHelp = true, description = "Print version information and exit.")
     boolean versionRequested;
 
-    public static void main(String[] args) {
-        CommandLine commandLine = new CommandLine(new App());
-        commandLine.parseArgs(args);
-        if (commandLine.isUsageHelpRequested()) {
-            commandLine.usage(System.out);
-        } else if (commandLine.isVersionHelpRequested()) {
-            commandLine.printVersionHelp(System.out);
+    @Override
+    public String call() throws Exception {
+        if (!filepath1.isAbsolute()) {
+            filepath1 = filepath1.toAbsolutePath();
         }
+
+        if (!filepath2.isAbsolute()) {
+            filepath2 = filepath2.toAbsolutePath();
+        }
+
+        var diff = Differ.generate(filepath1, filepath2);
+        System.out.println(diff);
+        return null;
     }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
 }
