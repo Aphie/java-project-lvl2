7187fa25d3807a261f9076a7c191ec5f2becc66e
s.andreyuk <s.andreyuk@crpt.ru>
2022-06-14 14:31:48 +0300

    - fix some linter issues in tests

--- src/test/java/hexlet/code/DifferTest.java
+++ src/test/java/hexlet/code/DifferTest.java
@@ -8,47 +8,47 @@ import org.junit.jupiter.api.Test;
 public class DifferTest {
 
     @Test
-    void differTest_Base () throws Exception {
+    void differTestBase() throws Exception {
         Path filePath1 = Paths.get("src/test/resources/differTestBase_file1.json");
         Path filePath2 = Paths.get("src/test/resources/differTestBase_file2.json");
 
-        String expected = "{\n" +
-                "  - follow: false\n" +
-                "    host: hexlet.io\n" +
-                "  - proxy: 123.234.53.22\n" +
-                "  - timeout: 50\n" +
-                "  + timeout: 20\n" +
-                "  + verbose: true\n" +
-                "}";
+        String expected = "{\n"
+                + "  - follow: false\n"
+                + "    host: hexlet.io\n"
+                + "  - proxy: 123.234.53.22\n"
+                + "  - timeout: 50\n"
+                + "  + timeout: 20\n"
+                + "  + verbose: true\n"
+                + "}";
         Assertions.assertEquals(expected, Differ.generate(filePath1,filePath2));
     }
 
     @Test
-    void differTest_firstFileEmpty () throws Exception {
+    void differTestFirstFileEmpty() throws Exception {
         Path filePath1 = Paths.get("src/test/resources/differTestFirstFileEmpty_file1.json");
         Path filePath2 = Paths.get("src/test/resources/differTestFirstFileEmpty_file2.json");
 
-        String expected = "{\n" +
-                "  + timeout: 20\n" +
-                "  + verbose: true\n" +
-                "}";
+        String expected = "{\n"
+                + "  + timeout: 20\n"
+                + "  + verbose: true\n"
+                + "}";
         Assertions.assertEquals(expected, Differ.generate(filePath1,filePath2));
     }
 
     @Test
-    void differTest_secondFileEmpty () throws Exception {
+    void differTestSecondFileEmpty() throws Exception {
         Path filePath1 = Paths.get("src/test/resources/differTestSecondFileEmpty_file1.json");
         Path filePath2 = Paths.get("src/test/resources/differTestSecondFileEmpty_file2.json");
 
-        String expected = "{\n" +
-                "  - host: hexlet.io\n" +
-                "  - timeout: 50\n" +
-                "}";
+        String expected = "{\n"
+                + "  - host: hexlet.io\n"
+                + "  - timeout: 50\n"
+                + "}";
         Assertions.assertEquals(expected, Differ.generate(filePath1,filePath2));
     }
 
     @Test
-    void differTest_bothFileEmpty () throws Exception {
+    void differTestBothFileEmpty() throws Exception {
         Path filePath1 = Paths.get("src/test/resources/differTestBothFileEmpty_file1.json");
         Path filePath2 = Paths.get("src/test/resources/differTestBothFileEmpty_file2.json");
 
@@ -57,49 +57,49 @@ public class DifferTest {
     }
 
     @Test
-    void differTest_bothFileSame () throws Exception {
+    void differTestBothFileSame() throws Exception {
         Path filePath1 = Paths.get("src/test/resources/differTestbothFileSame_file1.json");
         Path filePath2 = Paths.get("src/test/resources/differTestbothFileSame_file2.json");
 
-        String expected = "{\n" +
-                "    follow: false\n" +
-                "    host: hexlet.io\n" +
-                "    proxy: 123.234.53.22\n" +
-                "    timeout: 50\n" +
-                "}";
+        String expected = "{\n"
+                + "    follow: false\n"
+                + "    host: hexlet.io\n"
+                + "    proxy: 123.234.53.22\n"
+                + "    timeout: 50\n"
+                + "}";
         Assertions.assertEquals(expected, Differ.generate(filePath1,filePath2));
     }
 
     @Test
-    void differTest_allLinesDeleted () throws Exception {
+    void differTestAllLinesDeleted() throws Exception {
         Path filePath1 = Paths.get("src/test/resources/differTestAllLinesDeleted_file1.json");
         Path filePath2 = Paths.get("src/test/resources/differTestAllLinesDeleted_file2.json");
 
-        String expected = "{\n" +
-                "  - follow: false\n" +
-                "  - host: hexlet.io\n" +
-                "  - proxy: 123.234.53.22\n" +
-                "  - timeout: 50\n" +
-                "  + verbose: true\n" +
-                "}";
+        String expected = "{\n"
+                + "  - follow: false\n"
+                + "  - host: hexlet.io\n"
+                + "  - proxy: 123.234.53.22\n"
+                + "  - timeout: 50\n"
+                + "  + verbose: true\n"
+                + "}";
         Assertions.assertEquals(expected, Differ.generate(filePath1,filePath2));
     }
 
     @Test
-    void differTest_allLinesChanged () throws Exception {
+    void differTestAllLinesChanged() throws Exception {
         Path filePath1 = Paths.get("src/test/resources/differTestAllLinesChanged_file1.json");
         Path filePath2 = Paths.get("src/test/resources/differTestAllLinesChanged_file2.json");
 
-        String expected = "{\n" +
-                "  - follow: false\n" +
-                "  + follow: true\n" +
-                "  - host: hexlet.io\n" +
-                "  + host: hexlet\n" +
-                "  - proxy: 123.234.53.22\n" +
-                "  + proxy: 123.234.53.20\n" +
-                "  - timeout: 50\n" +
-                "  + timeout: 20\n" +
-                "}";
+        String expected = "{\n"
+                + "  - follow: false\n"
+                + "  + follow: true\n"
+                + "  - host: hexlet.io\n"
+                + "  + host: hexlet\n"
+                + "  - proxy: 123.234.53.22\n"
+                + "  + proxy: 123.234.53.20\n"
+                + "  - timeout: 50\n"
+                + "  + timeout: 20\n"
+                + "}";
         Assertions.assertEquals(expected, Differ.generate(filePath1,filePath2));
     }
 }
