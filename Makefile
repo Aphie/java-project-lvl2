373909a37f64885fed683406fb5588babad6a7c3
s.andreyuk <s.andreyuk@crpt.ru>
2022-06-14 14:18:31 +0300

    - add tests - fix some linter issues - add build-check.yml

--- Makefile
+++ Makefile
@@ -1,3 +1,12 @@
 # Makefile
 run-dist: # запуск исполняемого файла
 	./build/install/app/bin/app
+
+project-build: #сборка проекта
+	gradle clean build
+
+lint-main: #проверка на стандарт кодирования Main
+	gradle checkstyleMain
+
+lint-test: #сборка проекта
+	gradle checkstyleTest
--- src/main/java/hexlet/code/Differ.java
+++ src/main/java/hexlet/code/Differ.java
@@ -3,6 +3,7 @@ package hexlet.code;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
@@ -12,13 +13,27 @@ import com.fasterxml.jackson.databind.type.MapType;
 
 public class Differ {
     public static String generate(Path filePath1, Path filePath2) throws Exception {
-        Map<String, String> data1 = getData(Files.readString(filePath1));
-        Map<String, String> data2 = getData(Files.readString(filePath2));
+        Map<String, String> data1 = new HashMap<>();
+        Map<String, String> data2 = new HashMap<>();
+        String falseResult = new String();
+        Set<String> data1Keys = new HashSet<>();
+        Set<String> data2Keys = new HashSet<>();
+
+        if (!(Files.readString(filePath1)).isEmpty() && !(Files.readString(filePath2)).isEmpty()) {
+            data1 = getData(Files.readString(filePath1));
+            data2 = getData(Files.readString(filePath2));
+        } else if ((Files.readString(filePath1)).isEmpty() && !(Files.readString(filePath2)).isEmpty()) {
+            data2 = getData(Files.readString(filePath2));
+        } else if ((Files.readString(filePath2)).isEmpty() && !(Files.readString(filePath1)).isEmpty()) {
+            data1 = getData(Files.readString(filePath1));
+        } else {
+            return falseResult;
+        }
 
         TreeSet<String> resultKeys = new TreeSet<>();
 
-        Set<String> data1Keys = data1.keySet();
-        Set<String> data2Keys = data2.keySet();
+        data1Keys = data1.keySet();
+        data2Keys = data2.keySet();
         resultKeys.addAll(data1Keys);
         resultKeys.addAll(data2Keys);
 
