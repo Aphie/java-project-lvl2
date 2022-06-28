e701855890e4ad1835c920e2a0cb3ac8718b4e91
s.andreyuk <s.andreyuk@crpt.ru>
2022-06-28 13:08:30 +0300

    - fix linter errors

--- src/test/java/hexlet/code/ParserTest.java
+++ src/test/java/hexlet/code/ParserTest.java
@@ -15,14 +15,16 @@ public class ParserTest {
     @Test
     void parserTestBaseJson() throws Exception {
         Path filePath = Paths.get("src/test/resources/parserTest/parserTestBaseJson.json");
-        Map<String, String> expected = Map.of("host", "hexlet.io", "timeout", "50", "proxy", "123.234.53.22", "follow", "false");
+        Map<String, String> expected = Map.of("host", "hexlet.io", "timeout", "50",
+                "proxy", "123.234.53.22", "follow", "false");
         Assertions.assertEquals(expected, Parser.getData(filePath.toString(), Files.readString(filePath)));
     }
 
     @Test
     void parserTestBaseYml() throws Exception {
         Path filePath = Paths.get("src/test/resources/parserTest/parserTestBaseYml.yml");
-        Map<String, String> expected = Map.of("host", "hexlet.io", "timeout", "50", "proxy", "123.234.53.22", "follow", "false");
+        Map<String, String> expected = Map.of("host", "hexlet.io", "timeout", "50",
+                "proxy", "123.234.53.22", "follow", "false");
         Assertions.assertEquals(expected, Parser.getData(filePath.toString(), Files.readString(filePath)));
     }
 
@@ -33,7 +35,8 @@ public class ParserTest {
         Exception thrown = Assertions.assertThrows(Exception.class, () -> {
             Parser.getData(filePath.toString(), Files.readString(filePath));
         });
-        Assertions.assertEquals("ERROR: You entered filename without file format or tried to parse file with incorrect format", thrown.getMessage());
+        Assertions.assertEquals("ERROR: You entered filename without file format or tried to parse file with" +
+                " incorrect format", thrown.getMessage());
     }
 
     @Test
@@ -43,20 +46,23 @@ public class ParserTest {
         Exception thrown = Assertions.assertThrows(Exception.class, () -> {
             Parser.getData(filePath.toString(), Files.readString(filePath));
         });
-        Assertions.assertEquals("ERROR: You entered filename without file format or tried to parse file with incorrect format", thrown.getMessage());
+        Assertions.assertEquals("ERROR: You entered filename without file format or tried to parse file with" +
+                " incorrect format", thrown.getMessage());
     }
 
     @Test
     void parserTestSeveralDotsInName() throws Exception {
         Path filePath = Paths.get("src/test/resources/parserTest/parserTest.Several.DotsIn.Name.json");
-        Map<String, String> expected = Map.of("host", "hexlet.io", "timeout", "50", "proxy", "123.234.53.22", "follow", "false");
+        Map<String, String> expected = Map.of("host", "hexlet.io", "timeout", "50",
+                "proxy", "123.234.53.22", "follow", "false");
         Assertions.assertEquals(expected, Parser.getData(filePath.toString(), Files.readString(filePath)));
     }
 
     @Test
     void parserTestSeveralDotsInPath() throws Exception {
         Path filePath = Paths.get("src/test/resources/parserTest/.json/.yml/parserTestSeveralDotsInPath.yml");
-        Map<String, String> expected = Map.of("host", "hexlet.io", "timeout", "50", "proxy", "123.234.53.22", "follow", "false");
+        Map<String, String> expected = Map.of("host", "hexlet.io", "timeout", "50",
+                "proxy", "123.234.53.22", "follow", "false");
         Assertions.assertEquals(expected, Parser.getData(filePath.toString(), Files.readString(filePath)));
     }
 }
