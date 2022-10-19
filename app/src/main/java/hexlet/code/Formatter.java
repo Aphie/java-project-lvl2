149cdd438dd1e7e8f22347844f74e18629ca6069
s.andreyuk <s.andreyuk@crpt.ru>
2022-10-19 21:19:07 +0300

    - fix tests

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -1,6 +1,5 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import hexlet.code.formatters.JsonFormat;
 import hexlet.code.formatters.PlainFormat;
 import hexlet.code.formatters.StylishFormat;
